37e97ef6069071526ca6bd4aee64f8a79a96b5dc
Michal Gutowski <michal@cucumbersome.net>
2023-03-18 12:54:51 +0100

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.5.31"
     `kotlin-dsl`
     groovy
     `maven-publish`
-    id("java-gradle-plugin")
+    `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "1.1.0"
     id("com.github.ben-manes.versions").version("0.46.0")
     checkstyle
@@ -17,14 +17,26 @@ checkstyle {
 }
 
 group = "net.cucumbersome"
-version = "0.1-SNAPSHOT"
+version = "0.1"
 
 gradlePlugin {
-    plugins.create("jooqDockerComposePlugin") {
-        id = "net.cucumbersome.jooq-docker-compose"
-        implementationClass = "net.cucumbersome.jooqdockercomposeplugin.JooqDockerComposePlugin"
-        version = project.version
+
+    plugins {
+        create("jooqDockerComposePlugin") {
+            id = "net.cucumbersome.jooq-docker-compose"
+            implementationClass = "net.cucumbersome.jooqdockercomposeplugin.JooqDockerComposePlugin"
+            version = project.version
+            displayName = "Generate Jooq classes from docker-compose"
+        }
     }
+}
+pluginBundle {
+
+    website = "https://github.com/CucumisSativus/gradle-jooq-docker-compose"
+    vcsUrl = "https://github.com/CucumisSativus/gradle-jooq-docker-compose.git"
+    description =
+        "Plugin useses docker-compose to start database, migrate it with flyway and generate jooq classes"
+    tags = listOf("jooq", "docker-compose", "database", "flyway")
 }
 
 repositories {
@@ -47,4 +59,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
